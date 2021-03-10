b1a1349f4ba23e3f8bc2a298694481e4d8c2c963
Rafael Marçalo <63216029+rafa-99@users.noreply.github.com>
2021-03-10 00:04:00 +0000

    Fixed Up Toolchain Path

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export ARCH=arm64
-export CROSS_COMPILE=../gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=../gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf/bin/armv8l-linux-gnueabihf-
+export CROSS_COMPILE=../../gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=../../gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf/bin/armv8l-linux-gnueabihf-
 export CLANG_TRIPLE=aarch64-linux-gnu-
 mkdir -p build/modout
 
